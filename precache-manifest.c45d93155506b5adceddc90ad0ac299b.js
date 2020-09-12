b3a72bb3cff45ddd8b7a00f08a469d5cf1db73fa
liam027 <liam027@gmail.com>
2020-09-12 09:11:10 -0400

    Updates

---
+++ precache-manifest.c45d93155506b5adceddc90ad0ac299b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038d31fa0782460be6c6fa19813078b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3745b43b24acecbf3ac2",
+    "url": "/static/css/main.fcd5e5d0.chunk.css"
+  },
+  {
+    "revision": "089235ecbee854e71e09",
+    "url": "/static/js/2.f7d9b5c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7d9b5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3745b43b24acecbf3ac2",
+    "url": "/static/js/main.37bfc9b1.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  }
+]);
